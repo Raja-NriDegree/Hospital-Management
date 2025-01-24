6c4d2c59bb109a85e8ea4962a7eebfd3fd860e53
Raja-NriDegree <arsr141004@gmail.com>
2025-01-24 12:33:57 +0530

    Delete ass.py

--- ass.py
+++
@@ -1,8 +0,0 @@
-# Function to arrange numbers in ascending order
-def arrange_ascending(numbers):
-    return sorted(numbers)
-
-# Example usage
-numbers = [25, 4786, 441, 14]
-sorted_numbers = arrange_ascending(numbers)
-print("Numbers in ascending order:", sorted_numbers)
